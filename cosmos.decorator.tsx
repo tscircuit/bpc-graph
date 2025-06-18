3a6d3fd4c4612b7d895c2eabc2071f8a295ac807
seveibar <seveibar@gmail.com>
2025-06-17 20:23:58 -0700

    minor dec

--- cosmos.decorator.tsx
+++ cosmos.decorator.tsx
@@ -1,7 +1,7 @@
 export default ({ children }: any) => {
   return (
     <>
-      <script src="https://cdn.tailwindcss.com" />
+      <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
       {children}
     </>
   )
