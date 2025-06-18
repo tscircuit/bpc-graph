bf1388a44572fec55888cf3f576e4d186221e3bb
seveibar <seveibar@gmail.com>
2025-06-17 18:06:11 -0700

    wip bootstrap heuristic similarity distance

---
+++ lib/heuristic-similarity/getHeuristicSimilarityDistance.ts
@@ -0,0 +1,8 @@
+import type { CostConfiguration } from "lib/operations/configureOperationCostFn"
+import type { BpcGraph } from "lib/types"
+
+export const getHeuristicSimilarityDistance = (
+  g1: BpcGraph,
+  g2: BpcGraph,
+  costConfiguration: CostConfiguration,
+) => {}
