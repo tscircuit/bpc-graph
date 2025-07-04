9f29cf54d31d39394f897d10b645b8df54b1018a
Severin Ibarluzea <seveibar@gmail.com>
2025-07-04 08:19:06 -0700

    more dist views (#39)

--- lib/adjacency-matrix-network-similarity/getBpcGraphWlDistance.ts
+++ lib/adjacency-matrix-network-similarity/getBpcGraphWlDistance.ts
@@ -4,23 +4,30 @@ import { convertToFlatBpcGraph } from "../flat-bpc/convertToFlatBpcGraph"
 import { wlFeatureVec } from "./wlFeatureVec"
 import { getWlDotProduct } from "./wlDotProduct"
 
-const WL_DEGREES = 2
+const DEFAULT_WL_DEGREES = 2
 
-export const getWlFeatureVecs = (g: BpcGraph) => {
+export const getWlFeatureVecs = (
+  g: BpcGraph,
+  wlDegrees = DEFAULT_WL_DEGREES,
+) => {
   const flatBpc = convertToFlatBpcGraph(g as MixedBpcGraph)
 
   const { matrix, indexMapping } = getAdjacencyMatrixFromFlatBpcGraph(flatBpc)
 
-  return wlFeatureVec(matrix, WL_DEGREES, {
+  return wlFeatureVec(matrix, wlDegrees, {
     nodeInitialColors: indexMapping.map(
       (id) => flatBpc.nodes.find((n) => n.id === id)?.color ?? "_",
     ),
   })
 }
 
-export const getBpcGraphWlDistance = (g1: BpcGraph, g2: BpcGraph) => {
-  const wlVec1 = getWlFeatureVecs(g1)
-  const wlVec2 = getWlFeatureVecs(g2)
+export const getBpcGraphWlDistance = (
+  g1: BpcGraph,
+  g2: BpcGraph,
+  { wlDegrees = DEFAULT_WL_DEGREES }: { wlDegrees?: number } = {},
+) => {
+  const wlVec1 = getWlFeatureVecs(g1, wlDegrees)
+  const wlVec2 = getWlFeatureVecs(g2, wlDegrees)
 
-  return WL_DEGREES + 1 - getWlDotProduct(wlVec1, wlVec2)
+  return DEFAULT_WL_DEGREES + 1 - getWlDotProduct(wlVec1, wlVec2)
 }
