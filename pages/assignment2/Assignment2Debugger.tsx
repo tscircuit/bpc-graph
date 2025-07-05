699a6936a86d149a07e40a1f801d21e76e6fa677
seveibar <seveibar@gmail.com>
2025-07-05 14:13:15 -0700

    init debugger component

---
+++ pages/assignment2/Assignment2Debugger.tsx
@@ -0,0 +1,11 @@
+import { BpcGraph } from "lib/bpc-graph"
+
+export const Assignment2Debugger = ({
+  floatingGraph,
+  fixedGraph,
+}: {
+  floatingGraph: BpcGraph
+  fixedGraph: BpcGraph
+}) => {
+  // TODO
+}
