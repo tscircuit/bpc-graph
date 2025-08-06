eed222448a293f2c417156c0750460b5e2a6da08
Severin Ibarluzea <seveibar@gmail.com>
2025-08-05 22:16:44 -0700

    pin space partitioning (#69)
    * pin space partitioning
    
    * show error
    
    * finish pin grouping stage

---
+++ pages/partitioning/pin-gap-partitioning/pin-gap-partitioning01.page.tsx
@@ -0,0 +1,107 @@
+import { useMemo, useReducer, useState } from "react"
+import { RootCircuit } from "tscircuit"
+import { SchematicViewer } from "@tscircuit/schematic-viewer"
+import { InteractiveGraphics } from "graphics-debug/react"
+import { convertCircuitJsonToBpc } from "circuit-json-to-bpc"
+import { getGraphicsForBpcGraph } from "lib/index"
+import { PinSpacePartitionProcessor } from "lib/partition-processing/PinSpacePartitionProcessor"
+
+export default () => {
+  const [runCount, incRunCount] = useReducer((c) => c + 1, 0)
+  const [error, setError] = useState<string | null>(null)
+
+  const circuitJson = useMemo(() => {
+    const circuit = new RootCircuit()
+
+    circuit.add(
+      <board routingDisabled>
+        <chip
+          name="U1"
+          schPinArrangement={{
+            rightSide: {
+              direction: "top-to-bottom",
+              pins: [1, 2, 3, 4, 5, 6],
+            },
+          }}
+          schPinStyle={{
+            pin4: {
+              marginTop: 0.5,
+            },
+          }}
+        />
+      </board>,
+    )
+
+    return circuit.getCircuitJson()
+  }, [])
+
+  const bpcGraph = useMemo(() => {
+    return convertCircuitJsonToBpc(circuitJson, {
+      useReadableIds: true,
+    })
+  }, [circuitJson])
+
+  const graphics = useMemo(
+    () =>
+      getGraphicsForBpcGraph(bpcGraph, {
+        title: "Original BPC Graph",
+      }),
+    [bpcGraph],
+  )
+
+  const pinSpacePartitionProcessor = useMemo(() => {
+    return new PinSpacePartitionProcessor({
+      inputGraph: bpcGraph,
+      minGap: 0.25,
+      centerBoxId: "U1",
+      centerPinColors: ["component_center"],
+    })
+  }, [bpcGraph])
+
+  return (
+    <div>
+      <div style={{ paddingLeft: 16, paddingTop: 8 }}>Input</div>
+      <div style={{ display: "flex" }}>
+        <SchematicViewer
+          colorOverrides={{
+            schematic: {
+              background: "transparent",
+            },
+          }}
+          containerStyle={{
+            width: 500,
+            height: 600,
+          }}
+          circuitJson={circuitJson}
+        />
+        <InteractiveGraphics graphics={graphics} />
+      </div>
+      <div>
+        <div className="mt-2 mb-1 mx-2 flex justify-between">
+          <div>Pin Space Partitioning Toolbar</div>
+          <div className="flex gap-2">
+            <div>{pinSpacePartitionProcessor.iteration} iterations</div>
+            <div>stage: {pinSpacePartitionProcessor.stage}</div>
+          </div>
+        </div>
+        <div className="ml-1">
+          <button
+            className="py-1 p-2 border border-gray-300 ml-2"
+            onClick={() => {
+              try {
+                pinSpacePartitionProcessor.step()
+                incRunCount()
+              } catch (e) {
+                setError(e instanceof Error ? e.message : "Unknown error")
+              }
+            }}
+          >
+            Step
+          </button>
+        </div>
+      </div>
+      <div className="m-2 text-red-500">{error}</div>
+      <InteractiveGraphics graphics={pinSpacePartitionProcessor.visualize()} />
+    </div>
+  )
+}
